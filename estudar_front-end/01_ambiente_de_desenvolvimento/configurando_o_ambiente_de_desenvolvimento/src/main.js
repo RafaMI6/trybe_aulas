9af5788b0df137c1fe31f0432537e1a0af187080
Rafael <rs4003193@gmail.com>
2024-01-23 11:02:58 -0300

    project completed successfully

--- estudar_front-end/01_ambiente_de_desenvolvimento/configurando_o_ambiente_de_desenvolvimento/src/main.js
+++ estudar_front-end/01_ambiente_de_desenvolvimento/configurando_o_ambiente_de_desenvolvimento/src/main.js
@@ -1,9 +1,22 @@
 import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
+import './style.css';
 
 const button = document.querySelector('button');
+const main = document.querySelector('main');
 const displayPasswordEl = document.querySelector('h2');
 
+const randomPassword = nanoid();
+
+const message = 'Copiado com sucesso!';
+
 button.addEventListener('click', () => {
-  const randomPassword = nanoid();
   displayPasswordEl.innerHTML = randomPassword;
+});
+
+displayPasswordEl.addEventListener('click', () => {
+  copy(randomPassword);
+  const newH2 = document.createElement('h2');
+  newH2.innerHTML = message;
+  main.appendChild(newH2);
 });
